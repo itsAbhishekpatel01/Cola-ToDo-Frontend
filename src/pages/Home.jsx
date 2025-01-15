f1f761918b524f1a530412b023fe297d98af8f89
Abhishek Patel <itsabhishekpatel01@gmail.com>
2025-01-15 20:23:48 +0530

    Edit and Delete Feature added and UI enchanced

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import axios from 'axios'
+import BASE_URL from '../config';
+import { FaTrashAlt } from "react-icons/fa";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import { MdOutlineEdit } from "react-icons/md";
+import { FaCheck } from "react-icons/fa6";
+import { MdOutlineCheckBoxOutlineBlank } from "react-icons/md";
+import toast from 'react-hot-toast';
 
 
 
@@ -8,37 +15,73 @@ const Home = () => {
     const [todos, setTodos] = useState([]);
     const [task, setTask] = useState('');
     const [priority, setPriority] = useState('medium')
+    const [editId, setEditId] = useState(null);
+    const [editValue, setEditValue] = useState('')
 
     const fetchData = async()=>{
-        // backend se data fetch krne ke liye, use axios package
-        // earlier we used fetch, which comes with react
-        // const fetchedData = await axios.get('http://localhost:3000/api/todo');
-        const fetchedData = await axios.get('https://cola-todo-backend.onrender.com/api/todo');
-        setTodos(fetchedData.data.todos); 
+        try {
+            const fetchedData = await axios.get(`${BASE_URL}/todo/`);
+            setTodos(fetchedData.data.todos);
+        } catch (error) {
+            console.error('Error fetching todos:', error);
+        }
     }
 
-    const handleSubmit = async(e)=>{
-    e.preventDefault();
-    setTodos([...todos, {task, priority, status:'Pending'}]);
-    // const response = await axios.post('http://localhost:3000/api/todo/add',{task, priority});
-    const response = await axios.post('https://cola-todo-backend.onrender.com/api/todo/add',{task, priority});
-    setTask('');
-    setPriority('medium');
-}
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post(`${BASE_URL}/todo/add`, { task, priority });
+            setTodos([...todos, response.data.todo]); // Assuming the backend returns the added todo
+            setTask('');
+            setPriority('medium');
+            toast.success(`${response.data.message}`);
+        } catch (error) {
+            console.error('Error adding todo:', error);
+        }
+    };
+
+    const handleDelete = async (id) => {
+        try {
+            const response =  await axios.delete(`${BASE_URL}/todo/delete/${id}`);
+            setTodos(todos.filter(todo => todo._id !== id));
+            toast.success(`${response.data.message}`);
+        } catch (error) {
+            console.error('Error deleting todo:', error);
+        }
+    }
+
+    const handleEdit = async(todo)=>{
+        setEditId(todo._id);
+        setEditValue(todo.task)
+    }
+
+    const handleUpdate = async()=>{
+        const response = await axios.put(`${BASE_URL}/todo/update/${editId}`, {task:editValue});
+        toast.success(`${response.data.message}`);
+        fetchData();
+        setEditId(null);
+    }
+
 
     useEffect(()=>{
         fetchData();
     },[])
 
+    const getColor=(priority)=>{
+        if(priority==='low') return "text-green-500";
+        else if(priority==='medium') return "text-orange-500";
+        else return  "text-red-600";
+    }
+
 
 
     return (
-        <>
+        <div className='bg-slate-700 h-screen'>
             <Header />
             <div>
                 <form 
                     onSubmit={handleSubmit}
-                    className='w-screen justify-center flex mt-20 gap-2'>
+                    className='w-screen md:flex-row justify-center items-center flex flex-col pt-8 gap-4'>
                     <input type="text" placeholder='Add new task' required
                         value={task}
                         onChange={(e) => setTask(e.target.value)}
@@ -51,53 +94,41 @@ const Home = () => {
                         <option value="medium">Medium</option>
                         <option value="high">High</option>
                     </select>
-                    <button
-                        className='bg-pink-600  text-white px-4 py-1 rounded-lg active:scale-110 transition-all duration-300'>Add task</button>
+                    <button className='bg-pink-600  text-white px-4 py-1 rounded-lg active:scale-110 transition-all  duration-300'>Add task</button>
                 </form>
 
-                <div className='w-screen justify-center items-center flex mt-10 flex-col'>
-                    <h1>My Todos</h1>
+                <div className='w-screen  justify-center items-center flex mt-10'>
 
+                    <div className="shadow-md rounded-lg w-3/4 flex flex-col gap-3 px-5 py-3 bg-gray-200 ">
+                    {
+                        todos.length>0 && todos.map((todo)=>(
+                            <div key={todo._id} className='flex justify-between py-3'>
+                                <div className={`flex-1  flex gap-2 items-center ${getColor(todo.priority)}`}>
+                                    {/* <MdOutlineCheckBoxOutlineBlank className='text-black'/>  */}
+                                    {editId!=todo._id ? <p className='text-wrap'>{todo.task}</p>:
+                                    <input type="text" className='w-full outline-none' 
+                                    onChange={(e)=>{setEditValue(e.target.value)}} value={editValue} autoFocus
+                                    onKeyDown={e=>{if(e.key==="Enter") handleUpdate()}}/>}
+                                </div>
+                                <div className='flex items-center text-pink-600 gap-5'>
+                                    {editId==todo._id?<FaCheck onClick={handleUpdate}/>:
+                                    <MdOutlineEdit onClick={()=>{handleEdit(todo)}}
+                                    className='hover:-rotate-45 transition-all duration-300'/>}
+                                    <FaTrashAlt onClick={()=>{handleDelete(todo._id)}}
+                                        className='hover:scale-110 transition-all duration-300'/>
+                                </div>
+                            </div>
+                        ))
+                    }
+                    
 
-                    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-                        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                                <tr>
-                                    <th scope="col" className="px-6 py-3">
-                                        Task name
-                                    </th>
-                                    <th scope="col" className="px-6 py-3">
-                                        Priority
-                                    </th>
-                                    <th scope="col" className="px-6 py-3">
-                                        Status
-                                    </th>
-
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {todos.map((todo) => (
-                                    <tr key={todo._id} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                            {todo.task}
-                                        </th>
-                                        <td className="px-6 py-4">
-                                            {todo.priority}
-                                        </td>
-                                        <td className="px-6 py-4">
-                                            {todo.status}
-                                        </td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
                     </div>
 
 
                 </div>
 
             </div>
-        </>
+        </div>
     )
 }
 
